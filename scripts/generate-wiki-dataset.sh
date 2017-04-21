b96754a6845e70d63f617cb4826d4780f3272946
emrekgn <emrekaganakkaya@gmail.com>
2017-04-21 15:22:45 +0300

    do not attempt to download if the dump already exists

--- scripts/generate-wiki-dataset.sh
+++ scripts/generate-wiki-dataset.sh
@@ -15,9 +15,7 @@ PRJ_OWNER=$(ls -ld $PRJ_ROOT_PATH | awk '{ print $3":"$4 }')
 # Common variables
 #
 DATA_PATH="$PRJ_ROOT_PATH"/data/wiki
-echo $DATA_PATH
 SCRIPTS_PATH="$PRJ_ROOT_PATH"/scripts
-echo $SCRIPTS_PATH
 
 #
 # Install dependencies if necessary & create directory
@@ -28,12 +26,13 @@ echo "Checked dependencies."
 mkdir -p $DATA_PATH
 
 echo "Downloading Turkish Wikipedia dump..."
-wget https://dumps.wikimedia.org/trwiki/20170401/trwiki-20170401-pages-articles.xml.bz2 -P "$DATA_PATH"
+wget -nc https://dumps.wikimedia.org/trwiki/20170401/trwiki-20170401-pages-articles.xml.bz2 -P "$DATA_PATH"
 echo "Downloaded Turkish Wikipedia dump."
 
 echo "Extracting plain text..."
 bzcat "$DATA_PATH"/*.bz2 | "$SCRIPTS_PATH"/WikiExtractor.py -cb 250K -o "$DATA_PATH"/extracted -
 find "$DATA_PATH"/extracted -name '*bz2' -prune -exec bunzip2 -c {} \; > "$DATA_PATH"/wiki.csv
+rm -rf "$DATA_PATH"/extracted
 chown -R $PRJ_OWNER $DATA_PATH
 echo "Extracted plain text."
 
